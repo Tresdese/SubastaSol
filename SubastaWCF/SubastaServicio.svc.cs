bcc5eb364c0f080974dd1cd84a47b27295b40a43
tresdese <super_diegobros@hotmail.com>
2026-05-25 16:21:22 -0600

    Se removio .vs

--- SubastaWCF/SubastaServicio.svc.cs
+++ SubastaWCF/SubastaServicio.svc.cs
@@ -9,9 +9,11 @@ namespace SubastaWCF
 {
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "SubastaServicio" en el código, en svc y en el archivo de configuración a la vez.
     // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione SubastaServicio.svc o SubastaServicio.svc.cs en el Explorador de soluciones e inicie la depuración.
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single)]    
+    
     public class SubastaServicio : ISubastaServicio
     {
-        private List<ISubastaServicio> clientesConectados = new List<ISubastaServicio>();
+        private List<ISubastaCallback> clientesConectados = new List<ISubastaCallback>();
 
         private double ofertaMaximaActual = 0.0;
         private string liderActual = "Sin oferta";
@@ -19,14 +21,33 @@ namespace SubastaWCF
         public void EntrarSala(string nombreUsuario)
         {
             ISubastaCallback callback = OperationContext.Current.GetCallbackChannel<ISubastaCallback>();
-            if (!clientesConectados.Contains(callback))
-            {
 
+            if (clientesConectados.Contains(callback))
+            {
+                clientesConectados.Add(callback);
             }
+
+            callback.MensajeDelSistema($"Bienvenido, la oferta maxima actual es: {ofertaMaximaActual} por {liderActual}");
         }
 
         public void EnviarOferta(string nombreUsuario, double montoOferta)
         {
+            if (montoOferta > ofertaMaximaActual)
+            {
+                ofertaMaximaActual = montoOferta;
+                liderActual = nombreUsuario;
+                string mensajeHIstorial = $"Nueva oferta maxima de {nombreUsuario} por el monto {montoOferta}";
+                foreach (var cliente in clientesConectados) {
+                    try
+                    {
+                        cliente.NotificarNuevaOfertaMax(nombreUsuario, montoOferta);
+                        cliente.MensajeDelSistema(mensajeHIstorial);
+                    } catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
 
         }
     }
